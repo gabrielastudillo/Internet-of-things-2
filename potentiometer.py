47f7e9a39ae5249a786258f8526acdbd9920515a
gastudillo <47458556+gabrielastudillo@users.noreply.github.com>
2023-08-30 00:43:36 -0400

    Update potentiometer.py

--- potentiometer.py
+++ potentiometer.py
@@ -7,9 +7,9 @@ def init():
 
 def loop():
 	while True:
-		res = ADC0832.getResult()
+		res = ADC0832.getADC(0)
 		vol = 3.3/255 * res
-		print ('analog value: %03d  ||  voltage: %.2fV' %(res, vol))
+		print ('digital value: %03d  ||  voltage: %.2fV' %(res, vol))
 		time.sleep(0.2)
 
 if __name__ == '__main__':
